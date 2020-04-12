a9a8780ddc6122690d8e9523d6b3754282a64b41
André Figueirôa <alfigueiroab@gmail.com>
2020-04-12 13:15:09 -0300

    Updates

---
+++ precache-manifest.c569167a6af0652a7a7ffc24fe1ef851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fd21f406ca48f8def3d86819c1816b",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "97867992cffe5fe3b24a",
+    "url": "/movie-finder/static/css/main.29577159.chunk.css"
+  },
+  {
+    "revision": "ce62990311c6fe06a2a6",
+    "url": "/movie-finder/static/js/2.ecd2f0f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-finder/static/js/2.ecd2f0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97867992cffe5fe3b24a",
+    "url": "/movie-finder/static/js/main.bcb5e823.chunk.js"
+  },
+  {
+    "revision": "72ea36da1c02ef2a3f05",
+    "url": "/movie-finder/static/js/runtime-main.7b813dfe.js"
+  },
+  {
+    "revision": "70435b9e54f357e286b640962b6aa595",
+    "url": "/movie-finder/static/media/background.70435b9e.png"
+  }
+]);
